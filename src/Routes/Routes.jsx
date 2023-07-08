1e17b9e6268ab91b67e445b1213cfe8cc3ce87e6
ahad1033 <a.ahad2232@gmail.com>
2023-07-08 20:43:59 +0600

    code modified

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,9 +1,18 @@
 import {
-    createBrowserRouter,
-  } from "react-router-dom";
+  createBrowserRouter
+} from "react-router-dom";
+import Main from "../Layout/Main";
+import Home from "../Pages/Home/Home/Home";
 
-  export const router = createBrowserRouter([
+export const router = createBrowserRouter([
     {
-
-    }
+      path: "/",
+      element: <Main />,
+      children: [
+        {
+          path: "/",
+          element: <Home />
+        }
+      ]
+    },
   ]);
---
+++ src/Shared/NavBar/NavBar.jsx
@@ -0,0 +1,10 @@
+
+const NavBar = () => {
+    return (
+        <div>
+            <h3>this is navbar</h3>
+        </div>
+    );
+};
+
+export default NavBar;
